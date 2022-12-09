5c86ad0b21133d7b1231f2bb373192316c0aa29f
MirgorVlad <mirgorodskiy295@gmail.com>
2022-12-09 12:29:40 +0200

    DataSourceAutoConfiguration

--- HospitalProject/src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.hospitalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HospitalProjectApplication {
 
     public static void main(String[] args) {
