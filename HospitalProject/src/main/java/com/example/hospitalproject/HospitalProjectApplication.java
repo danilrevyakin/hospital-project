f78ee1fcebe435315c623299f71c0b89362815d9
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-09 18:38:07 +0200

    first scratch

--- HospitalProject/src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/HospitalProjectApplication.java
@@ -1,8 +1,16 @@
 package com.example.hospitalproject;
 
+import com.example.hospitalproject.medicalCard.MedicalCard;
+import com.example.hospitalproject.medicalCard.MedicalCardRepository;
+import com.example.hospitalproject.medicalCard.model.MedicalRecord;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HospitalProjectApplication {
@@ -11,4 +19,24 @@ public class HospitalProjectApplication {
         SpringApplication.run(HospitalProjectApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(MedicalCardRepository repository) {
+        MedicalRecord record = new MedicalRecord(
+                "Info",
+                "heart pain",
+                "valeriana",
+                LocalDate.now(),
+                null
+        );
+        return args ->{
+            MedicalCard medicalCard = new MedicalCard(
+                    4L,
+                    null,
+                    List.of("Takes drugsііі"),
+                    null,
+                    LocalDate.now()
+            );
+            repository.insert(medicalCard);
+        };
+    }
 }
