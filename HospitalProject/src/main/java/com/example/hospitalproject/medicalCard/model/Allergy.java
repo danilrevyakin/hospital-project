387ecb185791980b96fe7fcb9d6a896a9d71a0fe
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-10 22:37:23 +0200

    id refactor

--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/model/Allergy.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/model/Allergy.java
@@ -1,4 +1,20 @@
 package com.example.hospitalproject.medicalCard.model;
 
-public record Allergy (String title, String reaction){
+import com.mongodb.lang.Nullable;
+
+import java.util.Objects;
+
+public record Allergy (String title,@Nullable String reaction){
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Allergy allergy = (Allergy) o;
+        return Objects.equals(title, allergy.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
 }
--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/repository/MedicalCardRepository.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/repository/MedicalCardRepository.java
@@ -1,9 +1,11 @@
 package com.example.hospitalproject.medicalCard.repository;
 
-import com.example.hospitalproject.medicalCard.MedicalCard;
+import com.example.hospitalproject.medicalCard.model.MedicalCard;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.Optional;
 
 public interface MedicalCardRepository extends MongoRepository<MedicalCard, String>, CustomMedicalCardRepository {
-    Optional<MedicalCard> findMedicalCardBySqlKey(Long sqlKey);
+    @Override
+    Optional<MedicalCard> findById(String s);
 }
