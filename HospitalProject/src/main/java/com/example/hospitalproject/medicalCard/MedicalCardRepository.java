88b917c798f09f49c0d8697308b0f2fcecacef43
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-10 12:35:01 +0200

    update in mongo is working

--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/MedicalCardRepository.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/MedicalCardRepository.java
@@ -1,9 +1,9 @@
 package com.example.hospitalproject.medicalCard;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.Optional;
 
 public interface MedicalCardRepository extends MongoRepository<MedicalCard, String> {
     Optional<MedicalCard> findMedicalCardBySqlKey(Long sqlKey);
+
 }
