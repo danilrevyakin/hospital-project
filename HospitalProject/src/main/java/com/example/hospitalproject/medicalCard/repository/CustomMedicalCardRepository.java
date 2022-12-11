ffea98b0d3f6a29b924a50aa40d992fe4dee64e7
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-11 15:06:20 +0200

    expand interface functionality

--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/repository/CustomMedicalCardRepository.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/repository/CustomMedicalCardRepository.java
@@ -3,8 +3,23 @@ package com.example.hospitalproject.medicalCard.repository;
 import com.example.hospitalproject.medicalCard.model.Allergy;
 import com.example.hospitalproject.medicalCard.model.MedicalRecord;
 
+import java.util.List;
+
 public interface CustomMedicalCardRepository {
     void addAllergy(String id, Allergy allergy);
-    void addBadHabit(String id, String badHabit);
+
+    void updateAllergy(String id, String title, Allergy allergy);
+
+    void deleteAllergy(String id, String title, Allergy allergy);
+
     void addMedicalRecord(String id, MedicalRecord record);
+
+    void addBadHabit(String id, String badHabit);
+
+    void deleteBadHabit(String id, String badHabit);
+
+    void updateBadHabit(String id, String oldBadHabit, String newBadHabit);
+
+    void updateAllBadHabits(String id, List<String> newBadHabits);
+
 }
