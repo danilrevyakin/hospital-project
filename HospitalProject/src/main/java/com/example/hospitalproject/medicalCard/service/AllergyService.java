57b9e165fca8398dcdaf81315289b12085080654
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-14 18:51:15 +0200

    allergy service refactor

--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/service/AllergyService.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/service/AllergyService.java
@@ -17,18 +17,17 @@ public class AllergyService {
     private final MedicalCardRepository repository;
 
     public Set<Allergy> getAllergies(String id) {
-        if(repository.existsById(id)){
+        if (repository.existsById(id)) {
             return repository.getAllAllergiesById(id);
         }
         throw new MedicalCardNotFoundException();
     }
 
-
     public Set<Allergy> addAllergy(String id, Allergy allergy) {
         Optional<MedicalCard> cardOptional = repository.findById(id);
         if (cardOptional.isPresent()) {
             Set<Allergy> allergies = cardOptional.get().getAllergies();
-            if(allergies != null && allergies.contains(allergy)){
+            if (allergies != null && allergies.contains(allergy)) {
                 throw new IllegalStateException("There is already present " + allergy);
             }
             repository.addAllergy(id, allergy);
@@ -38,24 +37,21 @@ public class AllergyService {
     }
 
     public Set<Allergy> updateAllergy(String id, String title, Allergy allergy) {
-        Optional<MedicalCard> cardOptional = repository.findById(id);
-        if (cardOptional.isPresent()) {
-            if(!cardOptional.get().getAllergies().contains(new Allergy(title, null))){
-                throw new IllegalStateException("There is no " + title + " in " + id + " card");
-            }
-            repository.updateAllergy(id, title, allergy);
-            return repository.getAllAllergiesById(id);
-        }
-        throw new MedicalCardNotFoundException();
+        return modifyAllergyOperation(id, title,
+                () -> repository.updateAllergy(id, title, allergy));
     }
 
     public Set<Allergy> deleteAllergy(String id, String title) {
+        return modifyAllergyOperation(id, title, () -> repository.deleteAllergy(id, title));
+    }
+
+    private Set<Allergy> modifyAllergyOperation(String id, String title, Runnable runner) {
         Optional<MedicalCard> cardOptional = repository.findById(id);
         if (cardOptional.isPresent()) {
-            if(!cardOptional.get().getAllergies().contains(new Allergy(title, null))){
-                throw new IllegalStateException("There is no " + title + " in " + id + " card");
+            if (!cardOptional.get().getAllergies().contains(new Allergy(title, null))) {
+                throw new IllegalStateException("There is no " + title + " allergy in " + id + " card");
             }
-            repository.deleteAllergy(id, title);
+            runner.run();
             return repository.getAllAllergiesById(id);
         }
         throw new MedicalCardNotFoundException();
