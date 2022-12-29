f3c2c94f45dbc668ea3abce3d9562a9eed67a139
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-12-29 19:56:49 +0200

    testing of records repo

--- HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/model/MedicalRecord.java
+++ HospitalProject/src/main/java/com/example/hospitalproject/medicalCard/model/MedicalRecord.java
@@ -1,11 +1,15 @@
 package com.example.hospitalproject.medicalCard.model;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @Document
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicalRecord {
     private String info;
     private String symptoms;
---
+++ HospitalProject/src/test/java/com/example/hospitalproject/medicalCard/repository/MedicalRecordRepositoryTest.java
@@ -0,0 +1,213 @@
+package com.example.hospitalproject.medicalCard.repository;
+
+import com.example.hospitalproject.medicalCard.model.MedicalCard;
+import com.example.hospitalproject.medicalCard.model.MedicalRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static com.example.hospitalproject.medicalCard.repository.AllergyRepositoryTest.getField;
+
+@SpringBootTest
+class MedicalRecordRepositoryTest {
+
+    @Autowired
+    private MedicalRecordRepository recordRepository;
+    @Autowired
+    private MedicalCardRepository repository;
+    private final String id = "testID";
+
+    @Test
+    void getMedicalRecordsByIdAndDate() {
+    }
+
+    @Test
+    void getMedicalRecordsById() {
+        //given
+        LocalDateTime first = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        LocalDateTime second = first.plusSeconds(5);
+        MedicalRecord record = new MedicalRecord(
+                "Covid19",
+                "headache",
+                "vitamins",
+                "Richard",
+                first,
+                null
+        );
+
+        MedicalRecord record2 = new MedicalRecord(
+                "Covid20",
+                "headache and heart pain",
+                "vitamins",
+                "Brad",
+                second,
+                null
+        );
+
+        final MedicalCard card = new MedicalCard(
+                id,
+                null,
+                null,
+                List.of(record, record2),
+                null
+        );
+        repository.save(card);
+        //when
+        var actual = recordRepository.getMedicalRecordsById(id);
+        //then
+        final var expected = getRecords();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void addMedicalRecord() {
+        //given
+        LocalDateTime first = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        LocalDateTime second = first.plusSeconds(5);
+        LocalDateTime third = second.plusSeconds(5);
+        MedicalRecord record = new MedicalRecord(
+                "Covid19",
+                "headache",
+                "vitamins",
+                "Richard",
+                first,
+                null
+        );
+
+        MedicalRecord record2 = new MedicalRecord(
+                "Covid20",
+                "headache and heart pain",
+                "vitamins",
+                "Brad",
+                second,
+                null
+        );
+        MedicalRecord last = new MedicalRecord(
+                "Covid300",
+                "heart pain and mind issues",
+                "vitamins and good sleep",
+                "House",
+                third,
+                null
+        );
+
+        final MedicalCard card = new MedicalCard(
+                id,
+                null,
+                null,
+                List.of(record, record2),
+                null
+        );
+        repository.save(card);
+        //when
+        recordRepository.addMedicalRecord(id, last);
+        var actual = getRecords();
+        //then
+        final var expected = List.of(record, record2, last);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void updateMedicalRecord() {
+        //given
+        LocalDateTime first = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        LocalDateTime second = first.plusSeconds(5);
+        MedicalRecord record = new MedicalRecord(
+                "Covid19",
+                "headache",
+                "vitamins",
+                "Richard",
+                first,
+                null
+        );
+
+        MedicalRecord record2 = new MedicalRecord(
+                "Covid20",
+                "headache and heart pain",
+                "vitamins",
+                "Brad",
+                second,
+                null
+        );
+        MedicalRecord updated = new MedicalRecord(
+                "Covid300",
+                "heart pain and mind issues",
+                "vitamins and good sleep",
+                "House",
+                second,
+                null
+        );
+
+        final MedicalCard card = new MedicalCard(
+                id,
+                null,
+                null,
+                List.of(record, record2),
+                null
+        );
+        repository.save(card);
+        //when
+        recordRepository.updateMedicalRecord(id, second, updated);
+        var actual = getRecords();
+        //then
+        final var expected = List.of(record, updated);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void deleteMedicalRecord() {
+        //given
+        LocalDateTime first = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+        LocalDateTime second = first.plusSeconds(5);
+        LocalDateTime third = second.plusSeconds(5);
+        MedicalRecord record = new MedicalRecord(
+                "Covid19",
+                "headache",
+                "vitamins",
+                "Richard",
+                first,
+                null
+        );
+
+        MedicalRecord record2 = new MedicalRecord(
+                "Covid20",
+                "headache and heart pain",
+                "vitamins",
+                "Brad",
+                second,
+                null
+        );
+        MedicalRecord last = new MedicalRecord(
+                "Covid300",
+                "heart pain and mind issues",
+                "vitamins and good sleep",
+                "House",
+                third,
+                null
+        );
+
+        final MedicalCard card = new MedicalCard(
+                id,
+                null,
+                null,
+                List.of(record, record2, last),
+                null
+        );
+        repository.save(card);
+        //when
+        recordRepository.deleteMedicalRecord(id, second);
+        final var actual = getRecords();
+        //then
+        final var expected = List.of(record, last);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private List<MedicalRecord> getRecords(){
+        return getField(repository.findById(id), MedicalCard::getRecords);
+    }
+}
