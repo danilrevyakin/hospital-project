087de59c4d70ce553269ffea1df0939152f2a2ca
danylo_revyakin <danylo.revyakin.ext@competitoor.com>
2023-01-06 14:55:16 +0200

    Browser auto-launching implementation

---
+++ HospitalProject/src/main/java/com/example/hospitalproject/BrowserLauncher.java
@@ -0,0 +1,27 @@
+package com.example.hospitalproject;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.net.URI;
+
+@Component
+public class BrowserLauncher {
+    @EventListener(ApplicationReadyEvent.class)
+    public void launchBrowser(){
+
+        System.setProperty("java.awt.headless", "false");
+
+        Desktop desktop = Desktop.getDesktop();
+
+        try{
+            desktop.browse(new URI("http://localhost:8055/auth"));
+        }catch(Exception e){}
+
+    }
+
+}
+
+
